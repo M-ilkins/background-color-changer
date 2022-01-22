9c88a0d3140e273387b2535a0834228f94cefc91
Matthew Wilkins <wilkins52matthew@gmail.com>
2022-01-21 22:11:26 -0500

    A quick simple background color changer

---
+++ script.js
@@ -0,0 +1,13 @@
+const button = document.querySelector('button');
+
+function changeColor(target){
+    // using the RGB color value system
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    let styleString = `rgb(${r}, ${g}, ${b})`
+    document.body.style.backgroundColor = styleString;
+    console.log(styleString);
+}
+
+button.onclick = changeColor;
